f3a41198cf628f44749e464ef62acd7537179cb2
SelfishPuppy <stikleblit@gmail.com>
2023-11-28 16:06:24 +0200

    Refactor userController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,21 @@
 import { Controller, Delete, Get, Patch, UseGuards } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator/user.decorator';
 import { UserService } from './user.service';
-import { debug } from 'console';
 import { JwtGuard } from 'src/auth/guard';
 import { User } from '@prisma/client';
 import { UpdateUserDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  @UseGuards(JwtGuard)
   async findOne(@GetUser() user: User) {
     return user;
   }
 
   @Patch()
-  @UseGuards(JwtGuard)
   async update(@GetUser() user: User, dto: UpdateUserDto) {
     await this.userService.update(user, dto);
     return { message: 'User updated' };
